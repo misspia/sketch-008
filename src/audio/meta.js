2300bd247898403bfc3fd0a5b51cb4364f0fff3b
misspia <pia_leung@hotmail.com>
2023-03-05 21:03:16 +0000

    import sketch

---
+++ src/audio/meta.js
@@ -0,0 +1,6 @@
+export const BASE_URL = 'https://raw.githubusercontent.com/misspia/assets/master/audio/';
+
+export const audioMeta = {
+  url: `${BASE_URL}nurko-right-now.mp3`,
+  title: `Right Now - Nurko & Misdom` 
+ }
